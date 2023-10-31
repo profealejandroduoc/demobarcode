811ac3a0e6cd7f178d7554dbaf47ec66e9ff6808
El profe <al.sepulvedam@profesor.duoc.cl>
2023-10-31 09:34:49 -0300

    barcode listo

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Demo Barcode Scanner Ionic 7
+
+## Instalar barcode
+##### npm i phonegap-plugin-barcodescanner
+
+### Luego
+##### npm install @awesome-cordova-plugins/barcode-scanner
+
+### Importar en app.module.ts
+#### import {BarcodeScanner} from '@awesome-cordova-plugins/barcode-scanner/ngx'
+#### ....
+#### providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, BarcodeScanner],
+
+### En la página Home import
+#### import {BarcodeScanner} from '@awesome-cordova-plugins/barcode-scanner/ngx'
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {BarcodeScanner} from '@awesome-cordova-plugins/barcode-scanner/ngx'
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  texto:string=''
+  constructor(private barcodescanner:BarcodeScanner) {}
 
+  scan(){
+    this.barcodescanner.scan().then(barcodedata=>{
+      console.log("Scaneando...", barcodedata);
+      this.texto=(JSON.stringify(barcodedata));
+    }).catch(err=>{
+      console.log("ERROR AL ESCANEAR!!!!");
+    })
+
+  }
 }
